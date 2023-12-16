09fade524118bb1079dcaa6ba1033bab961d2993
Albert Nakpil <albertnakpil2796@gmail.com>
2023-12-16 23:48:59 +0800

    feat: #api-gateway implement #category-service retrieval of user categories

--- apps/api/src/modules/category/category.controller.ts
+++ apps/api/src/modules/category/category.controller.ts
@@ -1,14 +1,45 @@
-import { Controller, Get, Inject } from "@nestjs/common";
-import { ClientProxy } from "@nestjs/microservices";
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post, Req } from "@nestjs/common";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
+import { SkipThrottle } from "@nestjs/throttler";
+import { CreateCategoryDto, GetUserCategoriesDto } from "@spendee-clone/common/dto";
+import { Category, User } from "@spendee-clone/common/types";
+import { catchError, firstValueFrom, throwError, timeout } from "rxjs";
 
-@Controller()
+import { Authorize } from "../../decorators/authorize.decorator";
+
+@SkipThrottle()
+@Controller('categories')
 export class CategoryController {
   constructor(
     @Inject('CATEGORY_SERVICE') private readonly categoryServiceClient: ClientProxy
   ) {}
 
-  @Get('/categories/health')
+  @Get('/health')
   healthCheck() {
     return this.categoryServiceClient.send({ cmd: 'health' }, '');
+  }
+
+  @Get('/')
+  @Authorize()
+  @HttpCode(HttpStatus.OK)
+  async getUserWallets(@Req() req: { user: User }) {
+    return firstValueFrom(
+      this.categoryServiceClient
+        .send<Category[], GetUserCategoriesDto>({ cmd: 'get-user-categories', role: 'category' }, { userId: req.user.id })
+        .pipe(timeout(5000))
+        .pipe(catchError((error) => throwError(() => new RpcException(error))))
+    );
+  }
+
+  @Post('/')
+  @Authorize()
+  @HttpCode(HttpStatus.CREATED)
+  async createCategory(@Req() req: { user: User }, @Body() createCategoryDto: Pick<CreateCategoryDto, 'name' | 'type'> ) {
+    return firstValueFrom(
+      this.categoryServiceClient
+        .send<Category, CreateCategoryDto>({ cmd: 'create-category', role: 'category' }, { ...createCategoryDto, userId: req.user.id })
+        .pipe(timeout(5000))
+        .pipe(catchError((error) => throwError(() => new RpcException(error))))
+    );
   }
 }
